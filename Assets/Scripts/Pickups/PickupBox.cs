a6ef96b1d431cf9c45c4418a926aa9bf0fa2eb36
Josh Heisler <imatastywaffle2@gmail.com>
2016-02-02 11:29:18 -0800

    Added pickup box

---
+++ Assets/Scripts/Pickups/PickupBox.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupBox : MonoBehaviour {
+
+    GameObject PickupType;
+
+    // make sure only if tagged player
+        void OnTriggerEnter(Collider other)
+        {
+            Destroy(gameObject);
+        }   
+
+    
+}
