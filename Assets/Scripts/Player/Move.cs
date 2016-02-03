0a27d4dcdac7f823b59b624b48ec4c6928cba383
Yoskreth <lito@students.santiam.org>
2016-02-03 11:40:41 -0800

    Added InputInformation and Vehicle Scripts, updated move script

--- Assets/Scripts/Player/Move.cs
+++ Assets/Scripts/Player/Move.cs
@@ -2,12 +2,15 @@
 using System.Collections;
 
 public class Move : MonoBehaviour {
-    bool disabled;
-    bool inMotion;
-    float horizontalspeed;
-    float verticalspeed;
+    public GameObject Vehicle;
+    bool disabled; //Unable to move
+    bool inMotion; //Is this object in motion
+    float horizontalspeed; //How fast you can move side to side
+    float verticalspeed; //How fast you can ascend or descend
     float amplitude;
-    float fowardspeed; 
+    float fowardspeed;
+    float acceleration;
+    
 
     public Vector3 tempPosition;
 	// Use this for initialization
@@ -19,6 +22,9 @@ public class Move : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate ()
     {
+        acceleration = GetComponent<Vehicles>().fowardAccel;
+        horizontalspeed = GetComponent<Vehicles>().horizontalSpeed;
+        verticalspeed = GetComponent<Vehicles>().verticalSpeed;
         tempPosition.x += horizontalspeed;
         tempPosition.y = Mathf.Sin(Time.realtimeSinceStartup * verticalspeed) * amplitude;
         tempPosition.z += fowardspeed;
