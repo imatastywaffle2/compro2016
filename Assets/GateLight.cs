f0cd813a24b4d0f6d9becf5f884da26847422776
Justin Howell <HowellJu@students.santiam.org>
2016-02-09 11:23:25 -0800

    Added Shader Light

--- Assets/GateLight.cs
+++ Assets/GateLight.cs
@@ -3,12 +3,11 @@ using System.Collections;
 
 public class GateLight : MonoBehaviour {
 
-	// Use this for initialization
+
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 	
 	}
