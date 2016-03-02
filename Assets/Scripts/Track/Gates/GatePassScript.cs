7268ccf6be51cca7e43f6d27788786f3a345efc4
Aaron Coe <coeaa@students.santiam.org>
2016-03-02 11:23:16 -0800

    Awesome changes

---
+++ Assets/Scripts/Track/Gates/GatePassScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GatePassScript : MonoBehaviour
+{
+    List<Player> PlayersPassed = new List< Player>();
+
+    private int count;
+   // public GUIText countText;
+
+	void Start ()
+    {
+       // count = 0;
+      //  SetCountText ();
+	}
+
+    void Update ()
+    {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            if (!PlayersPassed.Contains(col.gameObject.GetComponent<Player>()))
+                PlayersPassed.Add(col.gameObject.GetComponent<Player>());
+
+          //  count = count + 1;
+           // SetCountText();
+
+        }
+
+        //void SetCountText ()
+            {
+         //   countText.text = "Count: " + count.ToString();
+        }
+
+    }
+
+}
