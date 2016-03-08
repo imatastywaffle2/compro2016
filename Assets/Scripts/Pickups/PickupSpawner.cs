ef3614db56305633f95b6fb6d2a898844ab5b32e
Josh Heisler <imatastywaffle2@gmail.com>
2016-03-08 10:45:14 -0800

    fixed pickups spawner script

--- Assets/Scripts/Pickups/PickupSpawner.cs
+++ Assets/Scripts/Pickups/PickupSpawner.cs
@@ -8,25 +8,23 @@ public class PickupSpawner : MonoBehaviour
 
 
     // Spawn Delay in seconds
-    public float interval = 30;
+    public float interval = 15;
     // Use this for initialization
     void Start ()
     {
         //if something to make it not spawn if one is already there
         InvokeRepeating("SpawnNext", interval, interval);
     }
-	void SpawnNext(int i)
+	void SpawnNext()
     {
+        int randomNumber = Random.Range(0, 3);
         if(SpawnedPickup == null)
-            SpawnedPickup = (GameObject)Instantiate(Prefab[i], transform.position, Quaternion.identity);
+            SpawnedPickup = (GameObject)Instantiate(Prefab[randomNumber], transform.position, Quaternion.identity);
     }
     void Update()
     {
-       
-        int randomNumber = Random.Range(0, 3);
-        //random number generator (0 between length of prefab)
-        if (SpawnedPickup == null)
-            SpawnNext(randomNumber);
+
+      
     }
 	
 }
