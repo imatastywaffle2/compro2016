f1ad2bc64d7ebdc6096dbb2b0eb8e08fce450abb
庭硕 苗 <miaotingshuo@gmail.com>
2016-04-07 10:46:45 -0700

    Updatemult

---
+++ Assets/Scripts/MainMenu/MultiplayerConnect.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MultiplayerConnect : Photon.MonoBehaviour
+{
+    public Button playButton;
+
+    public bool AutoConnect = true;
+
+    public byte Version = 1;
+
+    private bool ConnectInUpdate = true;
+     
+    public virtual void Start()
+    {
+        PhotonNetwork.autoJoinLobby = false;
+    }
+
+    public virtual void Update()
+    {
+        if (ConnectInUpdate && AutoConnect && !PhotonNetwork.connected)
+        {
+            Debug.Log("Update() was called by unity. Scene is loaded. Let's connect to the Photon Master Server. Calling: PhotonNetwork.ConnectUsingSettings(); ");
+
+            ConnectInUpdate = false;
+            PhotonNetwork.ConnectUsingSettings(Version + "." + SceneManagerHelper.ActiveSceneBuildIndex);
+
+           
+        }
+
+    }
+
+    public virtual void OnConnectedToMaster()
+    {
+        playButton.interactable = true;
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public virtual void OnPhotonRandomJoinFailed()
+    {
+        Debug.Log("OnPhotonRandomJoinFailed() was called by PUN. No random room available, so we create one. Calling: PhotonNetwork.CreateRoom( null, new RoomOptions() {maxPlayers = 8}, null);");
+        PhotonNetwork.CreateRoom(null, new RoomOptions() { maxPlayers = 8 }, null);
+    }
+
+    public void OnJoinedRoom()
+    {
+        Debug.Log("OnJoinedRoom() called by PUN. Now this client is in a room");
+        ChangeToScene("Master");
+    }
+
+    public void ChangeToScene(string sceneToChangeTo)
+    {
+        SceneManager.LoadScene(sceneToChangeTo);
+    }
+}
+
+       
