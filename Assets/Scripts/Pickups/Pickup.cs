401267a4a78753d83529bef041c4ac1dc891490a
petersonse <petersonse@students.santiam.org>
2016-04-19 11:27:37 -0700

    shield and rocket icons appear in game when picked up

--- Assets/Scripts/Pickups/Pickup.cs
+++ Assets/Scripts/Pickups/Pickup.cs
@@ -5,11 +5,13 @@ using System.Collections;
 public class Pickup : MonoBehaviour {
     Vehicle vehicle;
     InputInformation InputInfo;
+    PickupUI pickupUI;
 
     public void Start()
     {
         vehicle = GetComponent<Vehicle>();
         InputInfo = GetComponent<InputInformation>();
+        pickupUI = GameObject.FindObjectOfType<PickupUI>();
     }
 
     public void FixedUpdate()
@@ -17,10 +19,12 @@ public class Pickup : MonoBehaviour {
         if (InputInfo.UsePickup())
         {
             Use();
+            pickupUI.disableIcon();
         }
     }
 
     public virtual void Use()
-    {       
+    {
+        
     }
 }
