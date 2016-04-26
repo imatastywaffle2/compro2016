22f3a7c98328224b1c9d8893cdeb3c4a552dabc0
Yoskreth <lito@students.santiam.org>
2016-04-26 11:20:55 -0700

    fixing projectile and shield scripts etc etc.

--- Assets/Scripts/Projectiles/Projectile.cs
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -35,26 +35,32 @@ public class Projectile : MonoBehaviour
         }
         else
         {
-            gameObject.GetComponent<Rigidbody>().velocity = transform.forward * speed;
+            GetComponent<Rigidbody>().AddForce(transform.forward * speed);
         }
     }
 
     void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.tag == "Player" && other.gameObject.GetComponent<Player>().playerID 
-            != gameObject.GetComponent<Player>().playerID && other.gameObject.tag != "Untagged")
+        if (other.gameObject.layer != 8)
         {
             other.gameObject.GetComponent<Vehicle>().Stun();
             Destroy(gameObject);           
+        }
+        else
+        {
+            Destroy(gameObject);
         }
     }
     void OnTriggerEnter(Collider otherShip)
     {
         //make a code for detecting a ship that isnt yourself.
-        if (otherShip.gameObject.GetComponent<Player>().playerID 
-            != gameObject.GetComponent<Player>().playerID && otherShip.gameObject.tag == "Player" && trackTarget && otherShip.gameObject.tag != "Untagged")
+        if (otherShip.gameObject.layer != 8 && otherShip.gameObject.layer != 0)
         {
             target = otherShip.transform;
+        }
+        else
+        {
+            target = null;
         }
     }
 }
