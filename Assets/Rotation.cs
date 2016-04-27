545ca8266f66c49a0fd61864fba818e64021cae1
bearjew39 <Neufeldtja@students.santiam.org>
2016-04-27 11:49:12 -0700

    rotation of jupiter

---
+++ Assets/Rotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class RotateAndOrbit : MonoBehaviour
+{
+    public Transform target;
+    public float RotationSpeed = 100f;
+    public float OrbitDegrees = 1f;
+    void Update()
+    {
+        transform.Rotate(Vector3.up, RotationSpeed * Time.deltaTime);
+        transform.RotateAround(target.position, Vector3.up, OrbitDegrees);
+    }
+}
