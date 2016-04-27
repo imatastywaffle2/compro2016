7c3fd4e35e9199b3f1e77418958e187eea60b83e
bearjew39 <Neufeldtja@students.santiam.org>
2016-04-27 11:57:33 -0700

    reall rotation script done! And at the right speed

--- Assets/Rotaion.cs
+++ Assets/Rotaion.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class Rotaion : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    public Transform target;
+    public float RotationSpeed = 100f;
+    public float OrbitDegrees = 1f;
+    void Update()
+    {
+        transform.Rotate(Vector3.up, RotationSpeed * Time.deltaTime);
+        transform.RotateAround(target.position, Vector3.up, OrbitDegrees);
+    }
 }
