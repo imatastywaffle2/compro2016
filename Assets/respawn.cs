024370a0c5723bdb8828a6b41a2fbd580a2db580
bearjew39 <Neufeldtja@students.santiam.org>
2016-05-03 10:51:43 -0700

    if you hit the planet your going to respawn at the respawn area.. hizzy in the house

---
+++ Assets/respawn.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class respawn : MonoBehaviour
+{
+    public Transform respawnPosition;
+
+
+
+
+    void Start()
+    {
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Top")
+        {
+                 GetComponent<Rigidbody>().isKinematic = true;
+            bool falling = false;
+        }
+
+        if (other.gameObject.tag == "Platform")
+        {
+            transform.position = respawnPosition.position;
+        }
+    }
+}
