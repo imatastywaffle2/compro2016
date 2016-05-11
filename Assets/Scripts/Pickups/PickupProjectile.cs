88c48306ef394932262a7db64ba0d24e5786fe62
Yoskreth <lito@students.santiam.org>
2016-05-11 11:09:00 -0700

    Fixed projectile rotation

--- Assets/Scripts/Pickups/PickupProjectile.cs
+++ Assets/Scripts/Pickups/PickupProjectile.cs
@@ -6,7 +6,7 @@ public class PickupProjectile : Pickup {
 
     public override void Use()
     {
-        GameObject projectile = PhotonNetwork.Instantiate("Projectile", transform.position, transform.rotation);
+        GameObject projectile = PhotonNetwork.Instantiate("Projectile", transform.position, transform.rotation, 0);
         Destroy(this);
     }
 
