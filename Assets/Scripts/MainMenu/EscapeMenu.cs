f11f1165249395cb974e13c841a33839f838ddf7
Matt Lahmann <lahmannm@santiam.org>
2016-05-26 12:28:51 -0700

    Added ability to leave game through escape

---
+++ Assets/Scripts/MainMenu/EscapeMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EscapeMenu : MonoBehaviour {
+    public GameObject leaveButton;
+    CursorLockMode wantedMode;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            
+            leaveButton.SetActive(!leaveButton.activeInHierarchy);
+            if (leaveButton.activeInHierarchy)
+            {
+                wantedMode = CursorLockMode.Confined;
+                Cursor.lockState = wantedMode;
+                // Hide cursor when locking
+                Cursor.visible = true;
+            }
+            else
+            {
+                wantedMode = CursorLockMode.Locked;
+                Cursor.lockState = wantedMode;
+                // Hide cursor when locking
+                Cursor.visible = false;
+            }
+        }
+	}
+}
